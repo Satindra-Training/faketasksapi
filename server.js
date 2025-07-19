af9a2b4ea098f9bb38057a243ffab0ee345370db
Satindra-Training <78679406+Satindra-Training@users.noreply.github.com>
2025-07-19 14:00:04 +0530

    final changed

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+// Remove default static middleware that looks for `/public`
+// const middlewares = jsonServer.defaults();
+
+const middlewares = jsonServer.defaults({
+  static: './nonexistent' // or false if using 0.17.0+
+});
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running at http://localhost:${PORT}`);
+});
